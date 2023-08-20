f55c6412613cd5367c3a179aee321b8d77371a48
freshmenfish <contacteconet2023@gmail.com>
2023-08-20 11:44:39 -0500

    Update Imports

--- src/App.js
+++ src/App.js
@@ -1,6 +1,10 @@
 import './App.css';
 import Navbar from './components/Navbar/Navbar.jsx';
 import Footer from './components/Footer/Footer.jsx';
+import About from './components/About/About.jsx';
+import Volunteer from './components/Volunteer/Volunteer.jsx';
+import Organizations from './components/Organizations/Organizations.jsx';
+import Support from './components/Support/Support.jsx';
 import { Routes, Route } from 'react-router-dom';
 
 function App() {
