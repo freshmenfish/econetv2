cc76c99b4df3419a1d3e4fcba89af6a17cd92775
freshmenfish <contacteconet2023@gmail.com>
2023-08-20 11:40:13 -0500

    Add Routing

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
 import './App.css';
 import Navbar from './components/Navbar/Navbar.jsx';
+import Footer from './components/Footer/Footer.jsx';
+import { Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
       <Navbar/>
+        <Routes>
+          <Route path="/econetv2/" element={ <About/> } />
+          <Route path="/econetv2/volunteer" element={ <Volunteer/> } />
+          <Route path="/econetv2/organizations" element={ <Organizations/> } />
+          <Route path="/econetv2/support" element={ <Support/> } />
+        </Routes>
+      <Footer/>
     </div>
   );
 }
